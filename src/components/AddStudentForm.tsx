6d4d8d7910966a4fffd9fad4481ebf7aa696ed68
Dhruba Das <dhrubadas.dev@gmail.com>
2025-12-15 22:51:32 +0530

    student & teacher form edit

--- src/components/AddStudentForm.tsx
+++ src/components/AddStudentForm.tsx
@@ -1,6 +1,29 @@
 "use client";
 
+import { StudentFormType } from "@/lib/formType";
+import { studentFormSchema } from "@/lib/zodSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 const AddStudentForm = () => {
+	const { handleSubmit, control } = useForm({
+		resolver: zodResolver(studentFormSchema),
+		defaultValues: {
+			sFullName: "",
+			sEmail: "",
+			sGender: "",
+			sPhoneNumber: "",
+			teacherTableTId: "",
+		},
+		mode: "all",
+	});
+
+	const addStudentHandler = async (asData: StudentFormType) => {
+		await new Promise<void>((r) => setTimeout(r, 2000));
+
+		console.log(asData);
+	};
+
 	return <></>;
 };
 
--- src/components/AddTeacherForm.tsx
+++ src/components/AddTeacherForm.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { Controller, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { teacherFormSchema } from "@/lib/zodSchema";
 import { TeacherFormType } from "@/lib/formType";
+import { teacherFormSchema } from "@/lib/zodSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import { Button } from "./shadcnui/button";
 import { Field, FieldError, FieldLabel } from "./shadcnui/field";
 import { Input } from "./shadcnui/input";
 import {
@@ -33,7 +34,7 @@ const AddTeacherForm = () => {
 	return (
 		<form
 			onSubmit={handleSubmit(addteacherhandler)}
-			className="grid gap-4"
+			className="grid gap-6"
 			noValidate>
 			<Controller
 				name="tFullName"
@@ -60,40 +61,38 @@ const AddTeacherForm = () => {
 				render={({ field, fieldState }) => (
 					<Field data-invalid={fieldState.invalid}>
 						<FieldLabel htmlFor={field.name}>Subject Name</FieldLabel>
-						<Controller
-							name="tFullName"
-							control={control}
-							render={({ field, fieldState }) => (
-								<Field data-invalid={fieldState.invalid}>
-									<FieldLabel htmlFor={field.name}>Full Name</FieldLabel>
-									<Select
-										name={field.name}
-										value={field.value}
-										onValueChange={field.onChange}>
-										<SelectTrigger
-											id={field.name}
-											aria-invalid={fieldState.invalid}>
-											<SelectValue placeholder="Select Subject" />
-										</SelectTrigger>
-										<SelectContent position="item-aligned">
-											<SelectItem value="bengali">Bengali</SelectItem>
-											<SelectItem value="english">English</SelectItem>
-											<SelectItem value="math">Math</SelectItem>
-											<SelectItem value="history">History</SelectItem>
-										</SelectContent>
-									</Select>
-
-									{fieldState.invalid && (
-										<FieldError errors={[fieldState.error]} />
-									)}
-								</Field>
-							)}
-						/>
+						<Select
+							name={field.name}
+							value={field.value}
+							onValueChange={field.onChange}>
+							<SelectTrigger
+								id={field.name}
+								aria-invalid={fieldState.invalid}>
+								<SelectValue placeholder="Select Subject" />
+							</SelectTrigger>
+							<SelectContent position="item-aligned">
+								<SelectItem value="bengali">Bengali</SelectItem>
+								<SelectItem value="english">English</SelectItem>
+								<SelectItem value="math">Math</SelectItem>
+								<SelectItem value="history">History</SelectItem>
+								<SelectItem value="geography">Geography</SelectItem>
+								<SelectItem value="astrology">Astrology</SelectItem>
+								<SelectItem value="economics">Economics</SelectItem>
+								<SelectItem value="javascript">Javascript</SelectItem>
+								<SelectItem value="physics">Physics</SelectItem>
+							</SelectContent>
+						</Select>
 
 						{fieldState.invalid && <FieldError errors={[fieldState.error]} />}
 					</Field>
 				)}
 			/>
+
+			<Button
+				type="submit"
+				className="cursor-pointer">
+				Submit
+			</Button>
 		</form>
 	);
 };
